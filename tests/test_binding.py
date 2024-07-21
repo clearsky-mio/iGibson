cc563c5498524e5e963e9b26c238b0488ab995e7
hao <2964491850@qq.com>
2024-07-22 03:56:25 +0800

    open GLFW mode in Windows.

--- tests/test_binding.py
+++ tests/test_binding.py
@@ -3,13 +3,15 @@ import platform
 from igibson.render.mesh_renderer.get_available_devices import get_available_devices
 from igibson.render.mesh_renderer.mesh_renderer_settings import MeshRendererSettings
 
+import pytest
 
+@pytest.mark.skipif(platform.system() == "Windows", reason="Only works on Unix-like systems")
 def test_device():
     assert len(get_available_devices()[0]) > 0
 
 
 def test_binding():
-    if platform.system() == "Darwin":
+    if platform.system() == "Darwin" or platform.system() == "Windows":
         from igibson.render.mesh_renderer import GLFWRendererContext
 
         setting = MeshRendererSettings()
